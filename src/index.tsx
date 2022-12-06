87a246dc03d06a910418a2fe203e54e5cc7765ea
szkj-web-he <xuejie.he@datareachable.com>
2022-12-06 13:37:30 +0800

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./font.scss";
 import "./style.scss";
 
 import { ConfigYML, PluginComms } from "@possie-engine/dr-plugin-sdk";
+import JumpWrap from "./Components/JumpWrap";
 import Header from "./header";
 import MainContent from "./main";
 import { OptionProps } from "./type";
-import JumpWrap from "./Components/JumpWrap";
 
 // import vConsole from "vconsole";
 
@@ -28,51 +28,22 @@ export const comms = new PluginComms({
 const Main: React.FC = () => {
     /* <------------------------------------ **** STATE START **** ------------------------------------ */
     /************* This section will include this component HOOK function *************/
-    const [ready, setReady] = useState(false);
 
     /* <------------------------------------ **** STATE END **** ------------------------------------ */
     /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
     /************* This section will include this component parameter *************/
-
-    useEffect(() => {
-        const encNode = document.createElement("script");
-        const sha256Node = document.createElement("script");
-        const sha1Node = document.createElement("script");
-        const md5Node = document.createElement("script");
-        encNode.src = new URL("./js/enc-base64-min.js", import.meta.url).toString();
-        sha256Node.src = new URL("./js/hmac-sha256.js", import.meta.url).toString();
-        sha1Node.src = new URL("./js/HmacSHA1.js", import.meta.url).toString();
-        md5Node.src = new URL("./js/md5.js", import.meta.url).toString();
-        document.head.insertBefore(sha256Node, document.getElementsByTagName("meta")[0]);
-
-        encNode.onload = () => {
-            setReady(true);
-        };
-        sha256Node.onload = () => {
-            document.head.insertBefore(sha1Node, document.getElementsByTagName("meta")[0]);
-        };
-        sha1Node.onload = () => {
-            document.head.insertBefore(md5Node, document.getElementsByTagName("meta")[0]);
-        };
-        md5Node.onload = () => {
-            document.head.insertBefore(encNode, document.getElementsByTagName("meta")[0]);
-        };
-    }, []);
 
     /* <------------------------------------ **** PARAMETER END **** ------------------------------------ */
     /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
     /************* This section will include this component general function *************/
 
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
-    if (ready) {
-        return (
-            <JumpWrap hidden={{ x: true }} className="wrapper">
-                <Header />
-                <MainContent />
-            </JumpWrap>
-        );
-    }
-    return <></>;
+    return (
+        <JumpWrap hidden={{ x: true }} className="wrapper">
+            <Header />
+            <MainContent />
+        </JumpWrap>
+    );
 };
 /* <------------------------------------ **** FUNCTION COMPONENT END **** ------------------------------------ */
 comms.renderOnReady(<Main />);
